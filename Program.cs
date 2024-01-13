2bdb88806048e1859fdecee089f5f4bc8d19f11c
Maxim <Thickfod@gmail.com>
2024-01-14 01:26:49 +0300

    Написан код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество элементов в массиве");
+int amount = int.Parse(Console.ReadLine());
+
+string[] array = new string[amount];
+
+for(int i = 0; i < amount; i++)
+{
+    Console.WriteLine("Введите " + (i+1) + " элемент");
+    array[i] = Console.ReadLine();
+}
+
+int count = 1;
+
+for(int i = 0; i < amount; i++)
+{
+    if (array[i].Length <=3) count++; 
+}
+
+string[] final = new string[count];
+
+int j = 0;
+
+for(int i = 0; i < amount; i++)
+{
+    if (array[i].Length <=3) 
+        {
+        final[j] = array[i];
+        j++;
+        }
+}
+
+for(int i = 0; i < count; i++)
+{
+    Console.Write(final[i] + " ");
+}
